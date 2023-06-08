0def4a6c4335a4960f88550388c199f903da5ff6
Alex Gilsoul <54417319+AGilsoul@users.noreply.github.com>
2023-06-08 00:22:10 -0600

    working newton approximation

--- Python/NumericalMethods/NumericalMethods.py
+++ Python/NumericalMethods/NumericalMethods.py
@@ -28,8 +28,13 @@ class Methods:
                 for i in range(num_iterations):
                     deriv = Methods.approx_deriv(func, x)
                     x = round(Methods.linear_zero(deriv, (x, func(x))), 5)
-                if x not in zeros or num_attempts >= max_attempts:
+                num_attempts += 1
+                if x not in zeros:
+                    print('found zero')
                     zeros.append(x)
+                    break
+                if num_attempts >= max_attempts:
+                    print('exceeded attempts')
                     break
         return sorted(zeros)
 
@@ -47,13 +52,32 @@ class Methods:
     def calc_poly(coefs, x):
         return sum([coefs[i]*x**i for i in range(len(coefs))])
 
+    @staticmethod
+    def mult_poly(coefs_1, coefs_2):
+        new_coefs = [0 for _ in range((len(coefs_1))+(len(coefs_2)) - 1)]
+        for i in range(len(coefs_1)):
+            for j in range(len(coefs_2)):
+                cur_power = i + j
+                new_coefs[cur_power] += coefs_1[i] * coefs_2[j]
+        return new_coefs
+
 
 def function(x):
     return Methods.calc_poly([0, 2, 5, 3], x)
 
 
 val = -7
-print(f'polynomial 3x^3 - 5x^2 + 2x - 7 at x={val}: {Methods.calc_poly([-7, 2, 5, 3], val)}')
-print(f'approximate zero of polynomial: x={Methods.newton_approx(function, num_zeros=3, x_range=(-1, 0))}')
+print(f'polynomial 3x^3 + 5x^2 + 2x - 7 at x={val}: {Methods.calc_poly([-7, 2, 5, 3], val)}')
+print(f'approximate zeros of polynomial 3x^3 + 5x^2 + 2x - 7: x={Methods.newton_approx(function, num_zeros=3, x_range=(-1, 0))}')\
+
+test_poly = Methods.mult_poly([-7, 2, 5, 3], [1, -2, 3, -13, 2, 4])
+
+
+def new_func(x):
+    return Methods.calc_poly(test_poly, x)
+
+
+print(f'multiplying polynomial 3x^3 + 5x^2 + 2x - 7 by 4x^5 + 2x^4 + 13x^3 + 3x^2 - 2x + 1: {test_poly}')
+print(f'approximate zeros of polynomial 12x^8 + 26x^7 + 57x^6 + 50x^5 + 21x^4 - 92x^3 - 20x^2 + 16x - 7: {Methods.newton_approx(new_func, num_zeros=8, x_range=(-10, 10))}')
 
 
